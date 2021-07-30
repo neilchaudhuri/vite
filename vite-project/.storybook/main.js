309421b7f2be9a3a2f05a19a161e4b44db5ea08e
neilchaudhuri <neil@vidyasource.com>
2021-07-30 18:11:44 -0400

    Storybook

--- vite-project/.storybook/main.js
+++ vite-project/.storybook/main.js
@@ -1,13 +1,27 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "core": {
-    "builder": "storybook-builder-vite"
-  }
+	stories: ["../stories/**/*.stories.@(ts|tsx)"],
+	addons: [
+		{
+			name: "@storybook/addon-essentials",
+			options: {
+				backgrounds: false,
+			},
+		},
+		{
+			name: "@storybook/addon-a11y",
+			options: {},
+		},
+	],
+	webpackFinal: async config => {
+		const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test(".svg"))
+		fileLoaderRule.exclude = /\.svg$/
+
+		config.module.rules.push({
+			test: /\.svg$/,
+			enforce: "pre",
+			loader: require.resolve("@svgr/webpack"),
+		})
+
+		return config
+	},
 }
--- vite-project/.storybook/preview.js
+++ vite-project/.storybook/preview.js
@@ -1,9 +1,47 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	viewport: {
+		viewports: {
+			xs: {
+				name: "XS",
+				styles: {
+					height: "568px",
+					width: "320px",
+				},
+				type: "mobile",
+			},
+			sm: {
+				name: "SM",
+				styles: {
+					height: "896px",
+					width: "480px",
+				},
+				type: "mobile",
+			},
+			md: {
+				name: "MD",
+				styles: {
+					height: "1112px",
+					width: "768px",
+				},
+				type: "tablet",
+			},
+			lg: {
+				name: "LG",
+				styles: {
+					height: "1112px",
+					width: "1024px",
+				},
+				type: "desktop",
+			},
+			xl: {
+				name: "XL",
+				styles: {
+					height: "1112px",
+					width: "1280px",
+				},
+				type: "desktop",
+			},
+		},
+		defaultViewport: "xs",
+	},
 }
